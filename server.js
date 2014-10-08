541d63f78cfe91c2bc977752cc27d67209810526
videmsky <stuffaccounts@gmail.com>
2014-10-07 23:36:43 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('*', function(req, res){
+  res.sendfile('index.html', { root: './public' });
+});
+
+app.listen(port);
+console.log("App listening on port " + port);
