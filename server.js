820e9fafd5d13e2ed147109226647618c84401d7
videmsky <stuffaccounts@gmail.com>
2014-10-12 09:40:57 -0700

    house keeping

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendfile('index.html', { root: './public' });
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(req, res){
+//   res.sendfile('index.html', { root: './public' });
+// });
+
+app.get('*', function(req, res) {
+	res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 app.listen(port);
